488bcb4f758a0e2db3d24b15b79fcd48064de0a8
Richard KEMP <richard@kemp.pm>
2023-06-09 09:57:02 +0200

    chore(phpdev): xdebug.ini based on major version
    * Handle a default xdebug.ini based on the major version (2 or 3)
      installed.

--- specs/phpdev/docker/entrypoint.sh
+++ specs/phpdev/docker/entrypoint.sh
@@ -2,8 +2,11 @@
 
 set -e
 
-XDEBUG_INI=/usr/local/etc/php/conf.d/xdebug.ini
+XDEBUG_MAJOR_VERSION=$(echo $XDEBUG_VERSION | cut -d'.' -f1)
+
+XDEBUG_INI_DEFAULT=/opt/docker/xdebug${XDEBUG_MAJOR_VERSION}.ini
 XDEBUG_INI_CUSTOM=${XDEBUG_INI}.custom
+XDEBUG_INI=/usr/local/etc/php/conf.d/xdebug.ini
 
 : "${XDEBUG_IDE:=VSCODE}"
 : "${XDEBUG_PORT:=9003}"
@@ -11,6 +14,7 @@ XDEBUG_INI_CUSTOM=${XDEBUG_INI}.custom
 if [ -f $XDEBUG_INI_CUSTOM ]; then
   cp $XDEBUG_INI_CUSTOM $XDEBUG_INI
 else
+  cp $XDEBUG_INI_DEFAULT $XDEBUG_INI
   sed -i "s/xdebug.idekey=.*/xdebug.idekey=${XDEBUG_IDE}/" ${XDEBUG_INI}
   sed -i "s/xdebug.remote_port=.*/xdebug.remote_port=${XDEBUG_PORT}/" ${XDEBUG_INI}
   sed -i "s/xdebug.client_port=.*/xdebug.client_port=${XDEBUG_PORT}/" ${XDEBUG_INI}
